c4134a630093e4d130f02d2b31e467ec34c40e75
leducnhan6a <taixegacon4a@gmail.com>
2024-11-14 04:07:41 +0700

    Note Design - First Project

---
+++ Note Design - First Project/main.js
@@ -0,0 +1,50 @@
+
+
+// lấy realtime seconds
+// function time() {
+//   var d = new Date();
+//   var s = d.getSeconds();
+//   var m = d.getMinutes();
+//   var h = d.getHours();
+//   return s;
+// }
+
+
+
+// setInterval(function () {
+//     console.log(time());
+// }, 1000);
+
+
+var $ = document.querySelector.bind(document);
+var $$ = document.querySelectorAll.bind(document);
+
+// setInterval(() => {
+//    var date = new Date(); 
+// }, );
+
+var data = new Date();
+var today = data.toDateString();
+var hour = data.getHours();
+var minute = data.getMinutes();
+
+
+const app = {
+
+    // setInterval(() => {
+    //     const time = new Date();
+    //     var hour = time.getHours();
+    //     var minute = time.getMinutes();
+    //     console.log(hour, minute);
+    // }, 60000),
+    
+    //set time
+    // function clock() {
+    //     return 
+    // }
+}
+
+
+//function 
+
+
